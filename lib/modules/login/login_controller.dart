c85bb25ae9b59cbc8ac4340545b221c12effb35a
Jefferson Santos <jefferson.mauricio@cloudinfo.pt>
2022-05-01 00:15:48 +0100

    wip homePage Layout

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/models/user_model.dart';
 
 class LoginController {
-  final authControler = AuthController();
-
+  final authController = AuthController();
   Future<void> googleSignIn(BuildContext context) async {
     GoogleSignIn _googleSignIn = GoogleSignIn(
       scopes: [
@@ -13,10 +13,14 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authControler.setUser(context, response);
+      final user = UserModel(
+        name: response!.displayName!,
+        photoURL: response.photoUrl,
+      );
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
-      authControler.setUser(context, null);
+      authController.setUser(context, null);
       print(error);
     }
   }
--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,5 +1,5 @@
-import "package:flutter/material.dart";
-
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 
@@ -8,15 +8,16 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authController = AuthController();
+    authController.currentUser(context);
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
         children: [
           Center(child: Image.asset(AppImages.union)),
-          Center(child: Image.asset(AppImages.logoFull))
+          Center(child: Image.asset(AppImages.logoFull)),
         ],
-        alignment: Alignment.center,
-      )
+      ),
     );
   }
 }
